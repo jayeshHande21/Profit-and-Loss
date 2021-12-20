461e5ec998533452f9d167ea09b8f4da9fa066b8
Jayu <jayeshhande00@gmail.com>
2021-12-20 23:01:09 +0530

    edit stock

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ function calculate(initial,current,quantity){
     if(initial > current){
         const loss = (initial-current)* quantity
         const lossPercentage = loss / initial * 100;
-       ShowMessage(`Hey!! Your total loss is ${loss}, and the percentage is ${lossPercentage.toFixed(2)}% 😔😔😔`);
+       ShowMessage(`Hey!! Your total loss is ${loss}, and the LossPercentage is ${lossPercentage.toFixed(2)}% 😔😔😔`);
     }
     else if(current > initial){
         const profit = (current - initial) * quantity;
         const profitPercentage = profit / initial * 100
-        ShowMessage(`Hey Your profit is ${profit} and percentage is ${profitPercentage.toFixed(2)}%🥳🥳🥳`);
+        ShowMessage(`Hey Your profit is ${profit} and ProfitPercentage is ${profitPercentage.toFixed(2)}%🥳🥳🥳`);
     }else{
         ShowMessage(`No Pain No Gain And No Gain And No Pain😐😐`)
     }
