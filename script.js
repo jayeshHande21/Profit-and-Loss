5353a466fbf7e4c1d048b5de81bce6771cbbf256
Jayu <jayeshhande00@gmail.com>
2022-09-01 12:08:56 +0530

    change server

--- script.js
+++ script.js
@@ -6,38 +6,38 @@ const button = document.querySelector("#button");
 
 const output = document.querySelector("#output");
 
-function calculateProfitOrLoss(){
-    const ip = initialPrice.value;
-    const qty = total.value;
-    const cp = currentPrice.value
-    calculate(ip,cp,qty);
+function calculateProfitOrLoss() {
+  const ip = initialPrice.value;
+  const qty = total.value;
+  const cp = currentPrice.value;
+  calculate(ip, cp, qty);
 }
 
+function calculate(initial, current, quantity) {
+  if (initial > current) {
+    const loss = (initial - current) * quantity;
+    const lossPercentage = (loss / initial) * 100;
 
-function calculate(initial,current,quantity){
-    if(initial > current){
-        const loss = (initial-current)* quantity
-        const lossPercentage = loss / initial * 100;
-       ShowMessage(`Hey!! Your total loss is ${loss}, and the LossPercentage is ${lossPercentage.toFixed(2)}% 😔😔😔`);
-    }
-    else if(current > initial){
-        const profit = (current - initial) * quantity;
-        const profitPercentage = profit / initial * 100
-        ShowMessage(`Hey Your profit is ${profit} and ProfitPercentage is ${profitPercentage.toFixed(2)}%🥳🥳🥳`);
-    }else{
-        ShowMessage(`No Pain No Gain And No Gain And No Pain😐😐`)
-    }
-
-
+    ShowMessage(
+      `Hey!! Your total loss is ${loss}, and the LossPercentage is ${lossPercentage.toFixed(
+        2
+      )}% 😔😔😔`
+    );
+  } else if (current > initial) {
+    const profit = (current - initial) * quantity;
+    const profitPercentage = (profit / initial) * 100;
+    ShowMessage(
+      `Hey Your profit is ${profit} and ProfitPercentage is ${profitPercentage.toFixed(
+        2
+      )}%🥳🥳🥳`
+    );
+  } else {
+    ShowMessage(`No Pain No Gain And No Gain And No Pain😐😐`);
+  }
+  //   console.log(ShowMessage);
 }
 
-function ShowMessage(message){
-    output.innerText = message;
-   
-
+function ShowMessage(message) {
+  output.innerText = message;
 }
-
-
-
-
 button.addEventListener("click", calculateProfitOrLoss);
